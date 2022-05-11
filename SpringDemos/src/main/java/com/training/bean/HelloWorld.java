568e2c8974ea2e6a55ba097698a0448ebca433aa
radha <radhavkrishna1994@gmail.com>
2022-05-11 11:30:41 +0530

    updated

--- SpringDemos/src/main/java/com/training/bean/HelloWorld.java
+++ SpringDemos/src/main/java/com/training/bean/HelloWorld.java
@@ -3,22 +3,27 @@ package com.training.bean;
 public class HelloWorld {
 	
 	private String message;
+	private String message1;
 	
-	public HelloWorld(String message)
+	public HelloWorld(String message1)
 	{
-		this.message=message;
+		//System.out.println("Message 1 injected...");
+		this.message1=message1;
 	}
 	
 	public HelloWorld() { }
 	
 	public void setMessage(String message)
 	{
+		//System.out.println("Message  injected...with setter");
 		this.message = message;
 	}
 	
 	public String sayHello()
 	{
-		return message;
+		return message+" "+message1;
 	}
+	
+	
 
 }
