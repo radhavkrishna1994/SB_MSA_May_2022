c23b403e9f1b56fc162b8090fd99a98de35a2ce3
radha <radhavkrishna1994@gmail.com>
2022-05-12 09:46:21 +0530

    web demos updated

---
+++ SpringWebDemos/src/main/java/com/training/controllers/TestController.java
@@ -0,0 +1,34 @@
+package com.training.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@ResponseBody
+	@RequestMapping("/hello") //http://localhost:8080/SpringWebDemos/hello
+	public String sayHello()
+	{
+		return "Spring Web App";
+	}
+			
+	@RequestMapping("/") //http://localhost:8080/SpringWebDemos
+	public String login()
+	{
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/home") //http://localhost:8080/SpringWebDemos
+	public String home(@RequestParam("username")String username,@RequestParam("email")String email
+			,ModelMap map)
+	{
+		map.addAttribute("username", username);
+		map.addAttribute("email", email);
+		return "home.jsp";
+	}
+	
+}
--- SpringWebDemos/src/main/webapp/index.jsp
+++ SpringWebDemos/src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Login Page</h2>
+<form action="home" method="get">
+<input type="text" name="username" placeholder="Enter Username"/> <br>
+<input type="text" name="email" placeholder="Enter Email"/> <br>
+<input type="submit" value="send"/>
+
+</form>
 </body>
 </html>
