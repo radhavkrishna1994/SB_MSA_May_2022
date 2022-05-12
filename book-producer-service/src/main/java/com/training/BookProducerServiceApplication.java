9b8202b5eb72a877813ab672bd3e4af57ddba89e
radha <radhavkrishna1994@gmail.com>
2022-05-12 12:02:58 +0530

    updated

--- book-producer-service/src/main/java/com/training/BookProducerServiceApplication.java
+++ book-producer-service/src/main/java/com/training/BookProducerServiceApplication.java
@@ -1,13 +1,36 @@
 package com.training;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.training.config.MyConfiguration;
+import com.training.interfaces.ConfigurationProcessor;
+
+
+
 @SpringBootApplication
-public class BookProducerServiceApplication {
+public class BookProducerServiceApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookProducerServiceApplication.class, args);
 	}
+	
+	@Autowired
+	private ConfigurationProcessor config;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//System.out.println(config.getMessage());
+		//System.out.println(config.getStartValue());
+		
+		//System.out.println(config.getEndValue());
+		
+		config.process();
+	}
+	
+	
 
 }
---
+++ book-producer-service/src/main/java/com/training/config/MyConfiguration2.java
@@ -0,0 +1,32 @@
+package com.training.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.training.interfaces.ConfigurationProcessor;
+
+
+@Profile("test")
+@ConfigurationProperties("number")
+@Configuration
+public class MyConfiguration2 implements ConfigurationProcessor{
+
+	@Value("${message}")
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	
+	public void process() {
+		System.out.println("This is in test mode");
+		
+	}
+	
+	
+	
+	
+}
