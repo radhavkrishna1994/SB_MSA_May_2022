37db25d52feff11648618d24cff5ca3613beb3dd
radha <radhavkrishna1994@gmail.com>
2022-05-17 12:29:20 +0530

    updated

--- spring-boot-security-db_old/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into users(username,password,role) values('user1','user123','ROLE_USER')
-insert into users(username,password,role) values('admin1','admin123','ROLE_ADMIN')
-insert into users(username,password,role) values('admin2','admin123','ROLE_USER,ROLE_ADMIN')
