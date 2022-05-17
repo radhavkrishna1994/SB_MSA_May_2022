e237bd335780c3e712d24131397c483536c67e82
radha <radhavkrishna1994@gmail.com>
2022-05-17 11:53:39 +0530

    update

--- spring-boot-security-db/src/main/java/com/training/config/SecurityConfigurationDb.java
+++ spring-boot-security-db/src/main/java/com/training/config/SecurityConfigurationDb.java
@@ -2,11 +2,16 @@ package com.training.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.training.services.MyUserDetailsService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,25 +20,59 @@ import lombok.extern.slf4j.Slf4j;
 @EnableWebSecurity
 public class SecurityConfigurationDb extends WebSecurityConfigurerAdapter {
 	
-	//Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder()
+	{
+		return new PasswordEncoder()
+				{
+					@Override
+					public String encode(CharSequence rawPassword) {
+						log.info("Encode method");
+						return rawPassword.toString();
+					}
+
+					@Override
+					public boolean matches(CharSequence rawPassword, String encodedPassword) {
+						log.info("Matches method");
+						return rawPassword.equals(encodedPassword);
+					}
+			
+				};
+	}
+	
 	//Authentication
 	public void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 				log.info("In Authentication");
 	 
+				auth
+				.userDetailsService(myUserDetailsService)
+				.passwordEncoder(getPasswordEncoder());
+				
 		
 	}
 	
 	//Authorization
 	
-	/*
-	 * public void configure(HttpSecurity http) throws Exception {
-	 * log.info("In Authorization"); http .authorizeRequests()
-	 * .antMatchers("/user/**") .hasRole("USER") .antMatchers("/admin/**")
-	 * .hasRole("ADMIN") .and() .formLogin();
-	 * 
-	 * }
-	 */
+	public void configure(HttpSecurity http) throws Exception
+	{
+		log.info("In Authorization");
+		http
+		.csrf().disable()
+		.headers().frameOptions().disable()
+		.and()
+		.authorizeRequests()
+		.antMatchers("/user/**")
+		.hasRole("USER")
+		.antMatchers("/admin/**")
+		.hasRole("ADMIN")
+		.and()
+		.formLogin();
+		
+	}
 	
 
 }
---
+++ spring-boot-security-db/src/main/java/com/training/services/MyUserDetailsService.java
@@ -0,0 +1,60 @@
+package com.training.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.training.model.MyUser;
+import com.training.repo.UserRepo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.info("In userdetails Service...");
+		Optional<MyUser> opUser = userRepo.findById(username);
+		MyUser myUser=null;
+		
+		if(opUser.isPresent())
+		{
+			myUser = opUser.get(); // user2	user123	USER_ROLE,ADMIN_ROLE
+			
+			log.info("User Object :"+myUser);
+			// before java 8
+			/*String rolesString[]=myUser.getRole().split(",");
+			List<SimpleGrantedAuthority> list = new ArrayList<>();
+			
+			for(String role:rolesString)
+			{
+				list.add(new SimpleGrantedAuthority(role));
+			}*/
+			
+			// java 8
+			List<SimpleGrantedAuthority> list = Arrays.stream(myUser.getRole().split(","))
+												.map(SimpleGrantedAuthority::new)
+												.collect(Collectors.toList());
+
+			return new User(username, myUser.getPassword(), list);
+		}
+				
+		return null;
+	}
+
+}
