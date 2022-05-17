444b7370d5ac4ceeefc09c6c85926ae320abcc29
radha <radhavkrishna1994@gmail.com>
2022-05-17 11:01:26 +0530

    updated

---
+++ spring-boot-security-db/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(username,password,role) values('user1','user123','USER_ROLE')
+insert into users(username,password,role) values('admin','admin123','ADMIN_ROLE')
+insert into users(username,password,role) values('user2','user123','USER_ROLE,ADMIN_ROLE')
