22c5787912d0715897d449817a0d5c3bd150a251
radha <radhavkrishna1994@gmail.com>
2022-05-27 09:34:32 +0530

    hello proects added

---
+++ HelloService1/src/main/java/com/training/HelloService1Application.java
@@ -0,0 +1,26 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@RestController
+@RequestMapping("/helloservice1")
+@SpringBootApplication
+public class HelloService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloService1Application.class, args);
+	}
+//http://localhost:8080/hello1
+	@GetMapping("/hello1")
+	public String sayHello(/* @RequestHeader("Authorization") String requestHeaderValue */ )
+	{
+		return "hello1 - service:  ";// +requestHeaderValue ;
+	}
+}
